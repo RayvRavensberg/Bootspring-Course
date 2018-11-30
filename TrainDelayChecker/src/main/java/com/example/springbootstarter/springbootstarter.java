e7b53f0dc1688adfde780b208f54f60e603c6dfb
MrRay <rayvr@live.nl>
2018-11-30 20:48:42 +0100

    First project commit

---
+++ TrainDelayChecker/src/main/java/com/example/springbootstarter/springbootstarter.java
@@ -0,0 +1,12 @@
+package com.example.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogNiks {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NogNiks.class, args);
+	}
+}
