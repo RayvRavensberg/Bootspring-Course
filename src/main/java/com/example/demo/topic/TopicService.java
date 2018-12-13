a35377094e65eb83f77b2a0785fa27a766fddcc3
MrRay <rayvr@live.nl>
2018-12-13 18:01:17 +0100

    Moved git root directory

--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -21,7 +21,7 @@ public class TopicService {
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().isPresent(id).get;
     }
 
     public void addTopic(Topic topic) {
